607f2433c2ddd337a7d65126c8d666fca9a2d601
Felipe <felipepeter@web.de>
2019-08-10 23:38:43 +0200

    Use a backup class

--- main.py
+++ main.py
@@ -6,47 +6,66 @@ def main():
 
     database = "flashbackup-1905020007.pqb"
 
-    with create_connection(database) as conn:
-        print("1. Query all cards")
-        cards = get(conn, 'cards')                      # Key: Card ID
-        categories = get(conn, 'categories')            # Key: Category ID
-        category_assigns = get(conn, 'categoryassigns') # Key: Card ID
-        score_files = get_score_files(conn)             # Key: Scorefile name
+    backup = Backup(database)
 
 
-        pass
+    pass
 
 
-def create_connection(db_file):
-    try:
-        conn = sqlite3.connect(db_file)
-        return conn
-    except Error as e:
-        print(e)
-
-    return None
 
 
-def get(conn, table):
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM pleco_flash_{}".format(table))
-
-    # Takes the first column of the table as keys for a dictionary
-    return dict([(row[0], row[1:]) for row in cur.fetchall()])
 
 
-def get_score_files(conn):
-    score_file_defs = get(conn, 'scorefiles')
-
-    score_files = dict()
-
-    for i_score_file, score_file_def in score_file_defs.items():
-        score_file = get(conn, 'scores_{}'.format(i_score_file))  # Key: Card ID
-        score_files[score_file_def[0]] = score_file
-
-    return score_files
 
 
+
+
+class Backup():
+    def __init__(self, database_path):
+        self.path = database_path
+
+        with self.create_connection(self.path) as self.conn:
+            print("1. Query all cards")
+            self.cards = self.get('cards')  # Key: Card ID
+            self.categories = self.get('categories')  # Key: Category ID
+            self.category_assigns = self.get('categoryassigns')  # Key: Card ID
+            self.score_files = self.get_score_files()  # Key: Scorefile name
+        
+
+    @staticmethod
+    def create_connection(db_file):
+        try:
+            conn = sqlite3.connect(db_file)
+            return conn
+        except Error as e:
+            print(e)
+
+        return None
+
+    def get(self, table):
+        cursor = self.conn.cursor()
+        cursor.execute("SELECT * FROM pleco_flash_{}".format(table))
+
+        column_names = [elem[0] for elem in cursor.description]
+        # Takes the first column of the table as keys for a dictionary
+        return dict([(row[0], Entry(dict(zip(column_names[1:], row[1:])))) for row in cursor.fetchall()])
+
+    def get_score_files(self):
+        score_file_defs = self.get('scorefiles')
+
+        score_files = dict()
+
+        for i_score_file, score_file_def in score_file_defs.items():
+            score_file = self.get('scores_{}'.format(i_score_file))  # Key: Card ID
+            score_files[score_file_def.name] = score_file
+
+        return score_files
+
+
+class Entry(dict):
+    def __init__(self, *args, **kwargs):
+        super(Entry, self).__init__(*args, **kwargs)
+        self.__dict__ = self
 
 
 if __name__ == '__main__':
