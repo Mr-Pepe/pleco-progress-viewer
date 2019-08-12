a97ef354275b704c311a45f5441cbcdf5b4bbf7d
Felipe <felipepeter@web.de>
2019-08-13 00:32:50 +0200

    Set up bokeh

--- plecoviewer/main.py
+++ plecoviewer/main.py
@@ -1,19 +1,49 @@
-import os
-from plecoviewer.utils import Backup
+# myapp.py
+from plecoviewer.utils import Backup, BackupSummary, get_backups
+# from plecoviewer.callbacks import score_file_chooser_callback
+from random import random
+from functools import partial
+
+import bokeh
+from bokeh.layouts import column, layout
+from bokeh.models import Button, Select, DatetimeTickFormatter
+from bokeh.palettes import RdYlBu3
+from bokeh.plotting import figure, curdoc
 
 
-def main():
+def score_file_chooser_callback(attr, old, new):
+    global data
+    data = summary.score_files[new]
 
-    database_dir = "../backups"
+def update_plot():
 
-    backups = []
-    for root, _, fnames in os.walk(os.path.join(database_dir)):
-        valid_fnames = [fname for fname in fnames if fname.split('.')[-1] == 'pqb']
-
-        for i_fname, fname in enumerate(valid_fnames):
-            print('Loading Backup {}/{}'.format(i_fname + 1, len(valid_fnames)))
-            backups.append(Backup(os.path.join(root, fname)))
+    pass
 
 
-if __name__ == '__main__':
-    main()
+backups_dir = "backups"
+
+backups = get_backups(backups_dir)
+summary = BackupSummary(backups)
+data = summary.score_files['Reading']
+
+# create a plot and style its properties
+p = figure(x_axis_type='datetime', toolbar_location=None, title='Test')
+x = [[ts for ts in sorted(data[card]['scores'])] for (card, scores) in data.items()]
+y = [[score for (timestamp, score) in sorted(data[card]['scores'].items())] for (card, scores) in data.items()]
+p.multi_line(x, y)
+p.border_fill_color = 'white'
+p.background_fill_color = 'grey'
+p.outline_line_color = None
+p.grid.grid_line_color = None
+
+# create a callback that will add a number in a random location
+score_file_names = [fname for fname in summary.score_files.keys()]
+score_file_chooser = Select(title='Score file', value='', options=score_file_names)
+score_file_chooser.on_change("value", score_file_chooser_callback)
+
+# add a button widget and configure with the call back
+# button = Button(label=list(summary.score_files.keys())[0])
+# button.on_click(dropdown_callback)
+
+# inputs = column(select)
+curdoc().add_root(layout([[score_file_chooser, p]]))
--- plecoviewer/utils.py
+++ plecoviewer/utils.py
@@ -1,6 +1,8 @@
 import sqlite3
 from sqlite3 import Error
-
+import numpy as np
+from datetime import datetime
+import os
 
 class Backup():
     def __init__(self, database_path):
@@ -11,6 +13,12 @@ class Backup():
             self.categories = self.get('categories')  # Key: Category ID
             self.category_assigns = self.get('categoryassigns')  # Key: Card ID
             self.score_files = self.get_score_files()  # Key: Scorefile name
+
+            # Get timestamp of the backup from file name
+            _, fname = os.path.split(database_path)
+            timestamp = fname.split('-')[-1].split('.')[0]
+            self.timestamp = datetime.strptime(timestamp, '%y%m%d%H%M')
+
 
     @staticmethod
     def create_connection(db_file):
@@ -42,7 +50,44 @@ class Backup():
         return score_files
 
 
-class Entry(dict):
+class Container(dict):
+    def __init__(self, *args, **kwargs):
+        super(Container, self).__init__(*args, **kwargs)
+        self.__dict__ = self
+
+class Entry(Container):
     def __init__(self, *args, **kwargs):
         super(Entry, self).__init__(*args, **kwargs)
-        self.__dict__ = self
+
+
+class BackupSummary():
+    def __init__(self, backups):
+        self.score_files = dict()
+
+        for backup in backups:
+            timestamp = backup.timestamp
+
+            for score_file in backup.score_files.keys():
+                if score_file not in self.score_files:
+                    self.score_files[score_file] = dict()
+
+                for card in backup.score_files[score_file]:
+                    if card not in self.score_files[score_file]:
+                        self.score_files[score_file][card] = dict({'scores': dict(), 'reviewed': 0})
+
+                    self.score_files[score_file][card]['scores'][timestamp] = backup.score_files[score_file][card].score
+
+                    if backup.score_files[score_file][card].reviewed > self.score_files[score_file][card]['reviewed']:
+                        self.score_files[score_file][card]['reviewed'] = backup.score_files[score_file][card].reviewed
+
+
+def get_backups(backups_dir):
+    backups = []
+    for root, _, fnames in os.walk(os.path.join(backups_dir)):
+        valid_fnames = [fname for fname in fnames if fname.split('.')[-1] == 'pqb']
+
+        for i_fname, fname in enumerate(valid_fnames):
+            print('Loading Backup {}/{}'.format(i_fname + 1, len(valid_fnames)))
+            backups.append(Backup(os.path.join(root, fname)))
+
+    return backups
